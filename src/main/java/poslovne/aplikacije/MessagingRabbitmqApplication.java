18edc07db49f2f18021418c30b4f3979e336fe93
jovanastef <jovanastefanovic.002@gmail.com>
2026-04-02 17:20:11 +0200

    main application class

---
+++ src/main/java/poslovne/aplikacije/MessagingRabbitmqApplication.java
@@ -0,0 +1,11 @@
+package poslovne.aplikacije;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingRabbitmqApplication {
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(MessagingRabbitmqApplication.class, args);
+    }
+}
